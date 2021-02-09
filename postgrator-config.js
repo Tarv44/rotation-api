1ef18da5e4cf9d77711351cabed116afad24f6b8
Jonah Tarver <jonahdevine@gmail.com>
2021-02-09 15:47:22 -0800

    Updated db driver

--- postgrator-config.js
+++ postgrator-config.js
@@ -1,13 +1,11 @@
 require('dotenv').config();
 const { Client } = require('pg')
 
-const client = new Client({
+module.exports = {
   "migrationDirectory": "migrations",
-  "ssl": { rejectUnauthorized: false },
   "driver": "pg",
   "connectionString": (process.env.NODE_ENV === 'test')
     ? process.env.TEST_DATABASE_URL
     : process.env.DATABASE_URL,
-})
+}
 
-module.exports = client
