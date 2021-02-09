2f5c5c4137db7f796caa7fda78aeace14fbbfd5c
Jonah Tarver <jonahdevine@gmail.com>
2021-02-09 13:38:00 -0800

    pg config fix

--- postgrator-config.js
+++ postgrator-config.js
@@ -1,10 +1,13 @@
 require('dotenv').config();
+const { Pool } = require('pg')
 
-module.exports = {
+const pool = new Pool({
   "migrationDirectory": "migrations",
   "ssl": { rejectUnauthorized: false },
   "driver": "pg",
   "connectionString": (process.env.NODE_ENV === 'test')
     ? process.env.TEST_DATABASE_URL
     : process.env.DATABASE_URL,
-}
+})
+
+module.exports = pool
