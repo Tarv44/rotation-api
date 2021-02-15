f33db64358afd5e3bd3939a4f19f238db036bdcb
Jonah Tarver <jonahdevine@gmail.com>
2021-02-15 11:17:27 -0800

    Altered message length in rotation_comments

--- src/config.js
+++ src/config.js
@@ -3,5 +3,5 @@ module.exports = {
     NODE_ENV: process.env.NODE_ENV || 'development',
     DATABASE_URL: process.env.DATABASE_URL || "postgresql://jonahtarver@localhost/rotation",
     TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || "postgresql://jonahtarver@localhost/rotation-test",
-    CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || "https://rotationexchange.com"
+    CLIENT_ORIGIN: "http://localhost:3000" || process.env.CLIENT_ORIGIN || "https://rotationexchange.com"
 }
